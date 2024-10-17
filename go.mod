ee3ec7365890bfb7fd3b78bb4587e523e3142052
Karn Wong <karn@karnwong.me>
2024-10-17 09:24:21 +0700

    get repos name

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kahnwong/workspace-init
 
 go 1.23.2
+
+require github.com/cli/go-gh/v2 v2.11.0
+
+require github.com/cli/safeexec v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+
+	gh "github.com/cli/go-gh/v2"
+)
+
+type RepoList []struct {
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Println("Hello")
+	limit := 5 // [TODO] set to 300 later
+	repoList, _, err := gh.Exec("repo", "list", "--no-archived", "--limit", strconv.Itoa(limit), "--json", "name")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// unmarshal json
+	var repoListStruct RepoList
+	err = json.Unmarshal(repoList.Bytes(), &repoListStruct)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create repoListStruct slice
+	var repos []string
+	for _, repo := range repoListStruct {
+		repos = append(repos, repo.Name)
+	}
+
+	fmt.Println(repos)
 }
