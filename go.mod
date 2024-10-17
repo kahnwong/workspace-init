c777d077bc0113b656e5b56ac24ccb83ce0ef1d2
Karn Wong <karn@karnwong.me>
2024-10-17 13:04:39 +0700

    unmarshal yaml

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/kahnwong/workspace-init
 
 go 1.23.2
 
-require github.com/cli/go-gh/v2 v2.11.0
+require (
+	github.com/cli/go-gh/v2 v2.11.0
+	gopkg.in/yaml.v3 v3.0.1
+)
 
-require github.com/cli/safeexec v1.0.0 // indirect
+require (
+	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/kahnwong/workspace-init/github"
 )
 
+type RepoGroups struct {
+	Repos []struct {
+		Group string   `json:"group"`
+		Repos []string `json:"repos"`
+	} `json:"repos"`
+}
+
 func main() {
 	repos := github.GetRepos()
 	fmt.Println(repos)
+
+	// read yaml
+	configFile, err := os.ReadFile("repos.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal the YAML data into a Config struct
+	var repoGroups RepoGroups
+	err = yaml.Unmarshal(configFile, &repoGroups)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(repoGroups)
 }
